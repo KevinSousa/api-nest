7bfb46e9ed1e3b4e50bbb7bfbf1250cb1c97ced4
Kevin Sousa <kevinsmoura@hotmail.com>
2021-01-09 14:50:35 -0300

    Ajustando segurança

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,17 +10,18 @@ import { LivrosService } from './livros.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'kevin',
-      database: 'livraria',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadModels: true,
       synchronize: true,
     }),
-    SequelizeModule.forFeature([Livro])
+    SequelizeModule.forFeature([Livro]),
   ],
   controllers: [
     AppController,
